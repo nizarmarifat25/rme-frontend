0e6073fa9f9cad190bb0bba6b1cde6b4909c2d45
nizarpedia <muhammad.marifat@iconpln.co.id>
2025-02-15 18:06:56 +0700

    feat: init project

---
+++ src/components/cummons/PageHead/PageHead.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+interface PropTypes {
+    title?: string;
+}
+const PageHead = (props: PropTypes) => {
+    const {title = 'RME'} = props;
+    return (
+        <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>{title}</title>
+        </Head>
+    )
+}
+
+export default PageHead
