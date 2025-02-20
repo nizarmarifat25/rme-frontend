10d67f037fad512b97aa4e1da8296f955628cc60
nizarmarifat25 <nizarmarifat253@gmail.com>
2025-02-21 03:12:35 +0700

    ref: add proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/mrs/:path*", // Proxy endpoint
+        destination: "http://freddxant.my.id/mrs/:path*", // Target API
+      },
+    ];
+  },
 };
 
 export default nextConfig;
